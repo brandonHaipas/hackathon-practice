b17f079a4914fecc0249cd5fcfdf3a2fab81c1c1
Brandon Peña <brandon.pena96@gmail.com>
2025-01-02 19:36:00 -0300

    first commit

---
+++ bot.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+tools = [
+  {
+      "type": "function",
+      "function": {
+          "name": "get_weather",
+          "parameters": {
+              "type": "object",
+              "properties": {
+                  "location": {"type": "string"}
+              },
+          },
+      },
+  }
+]
+
+completion = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[{"role": "user", "content": "What's the weather like in Paris today?"}],
+  tools=tools,
+)
+
+print(completion.choices[0].message.tool_calls)
